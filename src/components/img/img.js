9c9ae319dbef690d9258bcccd958100988cbfd8e
herdyy69 <herdyansah203@gmail.com>
2022-10-06 13:43:15 +0700

    fix pages product 6 oct 2022

--- src/components/img/img.js
+++ src/components/img/img.js
@@ -8,16 +8,38 @@ function Img(Props) {
     document.getElementById(Props.id).src = Props.img1;
   }
 
-
   return (
     <div className="w-full md:w-1/2 lg:w-1/3 p-4">
       <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={Props.img1} alt="blog" id={Props.id} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
         <div className="p-6">
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{Props.title}</h1>
+          <div className="flex items-center flex-wrap ">
+            <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" href="/product" target="_blank" rel="noreferrer">
+              Learn More
+              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+            <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg className="w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                <path d="M22 4L12 14.01l-3-3"></path>
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-500 inline-flex items-center leading-none text-sm">
+              <svg className="w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                <path d="M21 21v-2a10 10 0 10-5.93-9.14"></path>
+                <path d="M3 3L12 12l1.41-1.41"></path>
+                <path d="M10 19L21 8"></path>
+              </svg>
+              6
+            </span>
+          </div>
         </div>
       </div>
-      
     </div>
   );
 }
--- src/pages/inpages/product.js
+++ src/pages/inpages/product.js
@@ -1,9 +1,63 @@
-import Card from "../../components/card/card";
-
 const Product = () => {
   return (
-    <div>
-      <Card />
+    <div className="flex flex-row flex-nowrap items-start justify-around">
+      <div className="container mx-auto px-7 py-5">
+        <div className="flex flex-col items-start justify-start">
+          <div className="w-auto p-4">
+            <div className="carousel w-full">
+              <div id="item1" className="carousel-item w-full">
+                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+              </div>
+              <div id="item2" className="carousel-item w-full">
+                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+              </div>
+              <div id="item3" className="carousel-item w-full">
+                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+              </div>
+              <div id="item4" className="carousel-item w-full">
+                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+              </div>
+            </div>
+            <div className="flex flex-row flex-wrap justify-start py-2 gap-1">
+              <a href="#item1" className="">
+                <img src="https://placeimg.com/800/200/arch" className="w-32" />
+              </a>
+              <a href="#item2" className="">
+                <img src="https://placeimg.com/800/200/arch" className="w-32" />
+              </a>
+              <a href="#item3" className="">
+                <img src="https://placeimg.com/800/200/arch" className="w-32" />
+              </a>
+              <a href="#item4" className="">
+                <img src="https://placeimg.com/800/200/arch" className="w-32" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto px-7 py-5">
+        <div className="flex flex-col items-start justify-center">
+          <h1 className="text-3xl font-bold text-center text-gray-800">Product Name</h1>
+          <p className=" text-gray-600">$99.99</p>
+          <hr className="w-full" />
+          <span className="my-1"></span>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Add to Cart</button>
+          <span className="my-1"></span>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Buy Now</button>
+          <span className="my-1"></span>
+          <p className=" text-gray-600 text-center w-full">MORE PAYMENT OPTIONS</p>
+          <hr className="w-full" />
+          <span className="my-3"></span>
+          <div className="flex flex-row flex-wrap items-center justify-center py-2 gap-1">
+            <p className=" text-gray-600">
+              "As we floated down the beautiful James River, past the spot where the Cape Fair dock now stands, on past the mouth of the James into the White River and Taneycomo little did I realize the change that was destined to take place. Camping
+              each night close to fresh spring water, cleaning and cooking our fish in this beautiful setting and under the stars was an experience for me never to be forgotten." - Jim Strimple 1922 Refuse to Sink - Classic Float Knife. Sure to be a
+              favorite for the boat dock, tackle box and canoe. Natural cork handle and rust-resistant metal components. 4" x 1¼" x ¾" closed with 2¾" stainless steel blade. Set includes: Cork Knife Survival Whistle 10 Pk Waterproof Matches + Striker
+              8' Paracord Type III + Keeper * Due to restrictions, this item is only available for ground shipping within the contiguous United States.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
